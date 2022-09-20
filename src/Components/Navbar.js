3d787db61a4d51e13930df271111f794fb58e508
Abdullah Aldzic <abdullah.aldzic@misija.io>
2022-09-20 14:53:15 +0200

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from '../Images/logo.png';
+
+export default function Navbar() {
+  return ( 
+    <nav>
+        <img src={logo} alt="Slika logo AIRBNB"/>
+    </nav>
+  )
+
+}
