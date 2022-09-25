ee15e7413a6b759c854477c6e0a2d9d094eaedaa
abdullah_aldzic <abdullah.aldzic@misija.io>
2022-09-26 00:08:46 +0200

    m

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Card from "./Components/Card";
+import card from "./Images/user2.jpg";
+
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
-      <Card />
+      {/* <Hero /> */}
+      <Card img={card}
+            rating = "5.0"
+            reviewCount ={6}
+            country = "BiH"
+            title = "Life Lessons with Katie Zeferes "
+            price = {136}
+            
+            />
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,19 +2,19 @@ import React from "react";
 import card from "../Images/user2.jpg";
 import star from "../Images/noprofilepic.png"
 
-export default function Card(){
+export default function Card(props){
     return (
        <div className="card">
-            <img src={card} alt="star" className="card--image"/>
+            <img src={props.img} alt="star" className="card--image"/>
             <div className="card--stats">
                 <img src={star} alt="Star" className="card--star"/>
-                <span>5.0</span>
-                <span className="gray">(6)</span >
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount})</span >
+                <span className="gray">{props.country}</span>
 
             </div>
-            <p>Life Lessons with Katie Zeferes </p>
-            <p><span className="bold">From $136 </span>/ person </p>
+            <p>{props.title} </p>
+            <p><span className="bold">From {props.price}$</span> / person </p>
        </div>
     )
 } 
